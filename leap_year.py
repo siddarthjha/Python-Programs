e535fdda84c0dcda373ee520ad6b7e27d242f9e0
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2019-09-02 23:09:36 +0530

    Create leap_year.py

---
+++ leap_year.py
@@ -0,0 +1,13 @@
+def is_leap(year):
+    leap = False
+    
+    # Write your logic here
+    if year % 4 == 0 :
+        if year % 100 == 0 :
+            if year % 400 !=0:
+                leap = False
+            else:
+                leap = True
+        else:
+            leap =True 
+    return leap
