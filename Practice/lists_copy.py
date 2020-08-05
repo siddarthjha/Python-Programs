088facbc4f9dd6b83cee9d26e2c70d85cf97e630
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-05 09:49:29 +0530

    Update lists_copy.py

--- Practice/lists_copy.py
+++ Practice/lists_copy.py
@@ -11,6 +11,19 @@ l1 = l[:]
 print(id(l1), id(l))
 print(l1, l)
 # It is good for only lists as soon we introduce sublists the same problem arises
+l2 = [1, 2, 3, [4, 5, 6]]
+l3 = l2[:]
+# Here if you want to change the first three elements there will be no problem.
+# But when you change the sublist then the problem arises
+l3[3][1] = 7
+print(l3, l2)
+# So to avoid the sublists problem we deepcopy module from copy
+from copy import deepcopy
+l3 = [1, 2, 3, [4, 5, 6]]
+l4 = deepcopy(l3)
+l3[3][7] = 7
+print(l3, l4)
+# Now the problem is solved and you can copy and update as you wish without any problem
 
 
       
